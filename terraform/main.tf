a2dd5676423c01c86f83776570d0111f9058ac2e
emmanuelangat <71602847+emmanuelangat@users.noreply.github.com>
2023-11-07 14:08:24 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,9 @@ terraform {
 
     region = "us-east-1"
 
+    access_key = var.access_key
+    secret_key = var.secret_key
+
   }
 }
 
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,12 @@ variable "name" {
   type    = string
 
 }
+
+variable "access_key" {
+  type = string
+}
+
+
+variable "secret_key" {
+  type = string
+}

c9ab6c4722b684f09b32c7041e202d148fdaf91c
emmanuelangat <71602847+emmanuelangat@users.noreply.github.com>
2023-11-07 14:10:51 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,6 @@ terraform {
 
     region = "us-east-1"
 
-    access_key = var.access_key
-    secret_key = var.secret_key
 
   }
 }
@@ -15,6 +13,10 @@ provider "aws" {
   # access_key = ""
   # secret_key = " "
   region = "us-east-1"
+
+  access_key = var.access_key
+  secret_key = var.secret_key
+
 
 }
 

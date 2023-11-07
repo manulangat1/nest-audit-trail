e6a13def9c6b4b1749c2d0902b1993ad991d17f3
emmanuelangat <71602847+emmanuelangat@users.noreply.github.com>
2023-11-07 14:17:26 +0300

    now

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+
+provider "aws" {
+  region = "us-east-1"
+
+  access_key = var.access_key
+  secret_key = var.secret_key
+
+
+}
+
 terraform {
   backend "s3" {
     bucket = "blog-cms-test"
@@ -9,16 +19,6 @@ terraform {
   }
 }
 
-provider "aws" {
-  # access_key = ""
-  # secret_key = " "
-  region = "us-east-1"
-
-  access_key = var.access_key
-  secret_key = var.secret_key
-
-
-}
 
 module "audit_ecs" {
   source = "./modules/ecs"
